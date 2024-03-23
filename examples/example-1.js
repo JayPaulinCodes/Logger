91a2fa4df3797d9dae90ecb806db3d91fbdc3a06
Jacob Paulin <jacobwpaulin@gmail.com>
2024-03-23 18:13:20 -0400

    feat(logger/file): Added ability to close the logger stream

--- examples/example-1.js
+++ examples/example-1.js
@@ -1,5 +1,33 @@
 const logger = require("../lib/index.js");
 
-const LOG = new logger.Logger();
+(async () => {
+    const sleep = (ms) => new Promise(resolve => { setTimeout(resolve, ms); });
+    const LOG = new logger.Logger({
+        output: {
+            file: {
+                enabled: true
+            }
+        }
+    });
 
-LOG.log("Test");
+    await sleep (5);
+    LOG.log("Test 1");
+    
+    let resC;
+    do {
+        const [ x, y ] = LOG.close();
+        console.log("CLOSE", x, y);
+        resC = x;
+    } while (!resC);
+    
+    LOG.log("Test 2");
+    
+    let resO;
+    do {
+        const [ x, y ] = LOG.open();
+        console.log("OPEN", x, y);
+        resO = x;
+    } while (!resO);
+    
+    LOG.log("Test 3");
+})()
