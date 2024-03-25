f222f9a5e88d4bd5f2f825b0ff6effa5e753f6c7
Jacob Paulin <jacobwpaulin@gmail.com>
2024-03-25 16:45:54 -0400

    fix(logger/config): Added maxFileAge to partial interface

--- src/interfaces/partials/PartialFileOutputOptions.ts
+++ src/interfaces/partials/PartialFileOutputOptions.ts
@@ -17,4 +17,12 @@ export interface PartialFileOutputOptions {
      * Default: 'yyyy-mm-dd'T'hh-MM-ss.log'
      */
     outputFileName?: string
+
+    /**
+     * Controls if files should be deleted after a specific age.
+     * Use a number to define the max age in ms, with -1 being infinite.
+     * 2592000000 = 30 days
+     * Default: null
+     */
+    maxFileAge?: number;
 }
